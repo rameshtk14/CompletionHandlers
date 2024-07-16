2b28c8ca500e28b6e9735746797ccf6eb7a6ee1c
Ramesh Tekumudi <ramesh2014tk@gmail.com>
2024-07-16 17:31:44 +0530

    Initial Commit

---
+++ Concurrency_CompletionHandlers/Concurrency_CompletionHandlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_CompletionHandlersApp.swift
+//  Concurrency_CompletionHandlers
+//
+//  Created by RAMESH on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_CompletionHandlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concurrency_CompletionHandlers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concurrency_CompletionHandlers
+//
+//  Created by RAMESH on 16/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
